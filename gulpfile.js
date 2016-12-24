44243fa55a2c0854eef5fe637feac5049a550071
yeffasol <yeffasol@mail.ru>
2016-12-24 10:30:42 +0300

    Add styles, scripts, images

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,91 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
+const posthtml = require('gulp-posthtml');
+const pug = require('gulp-pug');
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
+const gulpif = require('gulp-if');
+const sync = require('browser-sync').create();
+const cssnano = require('gulp-cssnano');
+let NODE_ENV = process.env.NODE_ENV || 'development';
 
-gulp.task('style', function () {
-  return gulp.src('src/style/style.css')
-    .pipe(postcss([
-      require("postcss-cssnext")({
-        browsers: ['last 10 version']
-      })
-    ]))
-    .pipe(gulp.dest('dest'))
 
+const assets = [
+  'src/images{,/*.+(jpg|png)}',
+  '!src/html{,/**}',
+  '!src/styles{,/**}',
+  '!src/scripts/script.js',
+];
+
+const processors = [
+  require('postcss-import')(),
+  require('postcss-easysprites')({
+    imagePath: './src/images/',
+    spritePath: './src/images'
+  }),
+  require("postcss-cssnext")({
+    browsers: ['last 10 version']
+  }),
+  require('postcss-custom-media')(),
+  require('postcss-sorting')({
+    'sort-order': 'csscomb'
+  }),
+  require('css-mqpacker')({
+    sort: true
+  })
+];
+
+gulp.task('styles', () => {
+  return gulp.src('./src/styles/style.css')
+    .pipe(gulpif(NODE_ENV === 'development',
+      sourcemaps.init()
+    ))
+    .pipe(plumber())
+    .pipe(postcss(processors))
+    .pipe(gulpif(NODE_ENV === 'development',
+      sourcemaps.write()
+    ))
+    .pipe(gulpif(NODE_ENV === 'production',
+      cssnano()
+    ))
+    .pipe(gulp.dest('./dest/styles'))
+    .pipe(sync.stream());
 });
+
+gulp.task('html', () =>  {
+  return gulp.src('src/html/pages/*.pug')
+    .pipe(pug({
+      pretty: '  '
+    }))
+    .pipe(gulp.dest('dest'))
+    .pipe(sync.stream());
+});
+
+gulp.task('server', () => {
+  sync.init({
+    notify: false,
+    open:false,
+    server: {
+      baseDir: 'dest'
+    }
+  });
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/html/**/*.pug', ['html']);
+  gulp.watch('src/styles/**/*.css', ['style']);
+  gulp.watch(assets, ['copy']);
+});
+
+gulp.task('copy', () => {
+  return gulp.src(assets)
+    .pipe(gulp.dest('dest/'))
+});
+
+gulp.task('default', [
+  'copy',
+  'html',
+  'styles',
+  'server',
+  'watch',
+]);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const path = require('path');
+module.exports = {
+
+  entry: {
+    index: path.resolve('./src/scripts/index.js')
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./dest/scripts/'),
+    library:['name']
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'stage-0']
+      }
+    }]
+  },
+  watch: NODE_ENV == 'development',
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+  devtool: NODE_ENV == 'development' ? "inline-module-source-map" : null,
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV)
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "common",
+      minChunks: 2
+    })
+  ]
+};
+
+if (NODE_ENV == 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: true,
+        unsafe: true
+      }
+    })
+  );
+}
