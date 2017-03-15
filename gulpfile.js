b6f87c96dbe80150dba9c1121dd6cb1b95b823ee
yeffasol <yeffasol@mail.ru>
2017-03-15 13:44:46 +0300

    Add gradient

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,7 @@ let processors = [
       imagePath: './src/images/',
       spritePath: './src/images'
     }),
+    require('postcss-filter-gradient'),//поддержка градиентов ниже ie9
     require("postcss-color-rgba-fallback"),//добавляет цвет если нет поддержки прозрачности
     require('postcss-rgb-plz'),//конвертирует hex в rgb
     require('postcss-assets')({
